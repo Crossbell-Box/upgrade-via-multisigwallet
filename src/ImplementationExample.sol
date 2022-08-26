0b75ace50915c962a0d48c4aa5ce8d873df6a71b
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-08-26 23:42:18 +0800

    upgradeable proxy & implementation example

---
+++ src/ImplementationExample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (proxy/transparent/ProxyAdmin.sol)
+
+pragma solidity 0.8.10;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract ImplementationExample is ERC20("Token", "TOK", 18) {
+
+}
