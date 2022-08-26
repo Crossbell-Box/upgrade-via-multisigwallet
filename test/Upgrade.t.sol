d41bccb72b57ee89625fadf8a8d1ba82caa7c50a
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-08-26 23:48:00 +0800

    add upgrade test

---
+++ test/Upgrade.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "../src/ImplementationExample.sol";
+import "../src/ProxyAdmin.sol";
+import "../src/TransparentUpgradeableProxy.sol";
+
+contract UpgradeTest is Test {
+    ImplementationExample implementationExample;
+    ProxyAdmin proxyAdmin;
+    TransparentUpgradeableProxy transparentUpgradeableProxy;
+
+    function setUp() public {
+        implementationExample = new ImplementationExample();
+        proxyAdmin = new ProxyAdmin();
+        transparentUpgradeableProxy = new TransparentUpgradeableProxy(address(implementationExample),address(proxyAdmin),new bytes(0));
+    }
+
+    function testGetAdmin() public {
+        address admin = proxyAdmin.getProxyAdmin(transparentUpgradeableProxy);
+    }
+}
